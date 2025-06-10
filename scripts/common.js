5343afed8b4b191a82343d1762d7d8a1779d063a
Gabriele256 <gabrielerossi256@gmail.com>
2025-06-10 14:46:49 +0200

    prova 2

--- scripts/common.js
+++ scripts/common.js
@@ -13,7 +13,7 @@ for (const [id, path] of Object.entries(navigationMap)) {
     const element = document.getElementById(id);
     if (element) {
         element.addEventListener("click", () => {
-            window.location.href = `index.html`;
+            window.location.href = path;
         });
     }
 }
